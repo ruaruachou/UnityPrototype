3566e60d0279e873a6197358d3d0fcc7fcb445b3
John Doe <johndoe@example.com>
2023-05-13 02:50:37 +0800

    5-13

--- Assets/Scripts/CameraContral.cs
+++ Assets/Scripts/CameraContral.cs
@@ -6,7 +6,8 @@ public class CameraContral : MonoBehaviour
 {
     public Camera p1Camera1rd;
     public Camera p1Camera3rd;
-    
+
+
     private void Start()
     {
         p1Camera1rd.enabled = false;
@@ -16,10 +17,19 @@ public class CameraContral : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)) 
+        SwitchCameraState();
+    }
+    public void SwitchCameraState()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            p1Camera3rd.enabled=!
-                p1Camera1rd.enabled=!
+            p1Camera1rd.enabled = false;
+            p1Camera3rd.enabled = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            p1Camera1rd.enabled = true;
+            p1Camera3rd.enabled = false;
         }
     }
 }
